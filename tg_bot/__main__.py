3ae1da9d9cdbbb83951edbf1c55faa3cdfa64279
SL GEEK SHOW <83456156+supunmadurangasl@users.noreply.github.com>
2021-05-22 17:06:33 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -146,7 +146,7 @@ def start(bot: Bot, update: Update, args: List[str]):
                                                                                    [InlineKeyboardButton(text="ADD MOHINI TO YOUR GROUP",
                                                                        url="t.me/{}?startgroup=true".format(bot.username)),
                                                                                    InlineKeyboardButton(text="Source Code",
-                                                                       url="https://github.com/legendx22/mohini")
+                                                                       url="https://github.com/legendx22/mohininew")
                                                                                  ]]))
 
     else:
